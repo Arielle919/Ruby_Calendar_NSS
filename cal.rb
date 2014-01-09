d44a7249027c02705aff42362a760af238a18540
Arielle Adams <arielle919@gmail.com>
2014-01-08 22:12:36 -0600

    sub class setup and ready

--- cal.rb
+++ cal.rb
@@ -1,36 +1,45 @@
+# month = ARGV[0]
+# year = ARGV[1]
+
+class Calendar
+  attr_reader :m, :y
+
+  def initialize(m, y)
+    @m = m
+    @y = y
+  end
+
+  def leap_year
+    if ( @y % 4 == 0 || @y % 400 == 0 )
+      return true
+    elsif ( @y % 100 != 0  )
+      return false
+    end
+  end
+
+  def zeller(m, d)
+    months = ["march", "april", "may", "june", "july", "august", "september", "october", "november", "december", "january", "february"]
+    weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    h = d
+    mo = months.index(m.downcase) + 1 #makes 12 months
+    yr = (mo <= 10) ? y : y-1
+    k = yr % 100
+    x = yr / 100
+    formula = (h + (((13*mo) - 1) / 5).floor + k + (k/4).floor + (x/4).floor - (2*x)) % 7
+    weekdays[formula]
+  end
+end
+
+class Calendar_work < Calendar
 month = ARGV[0]
 year = ARGV[1]
+  attr_reader :m, :y, :d, :year, :month
 
-# class Calendar
-#   attr_reader :month, :year
-
-#   def initialize(month, year)
-#     @month = month
-#     @year = year
-#   end
-
-#   def leap_year
-#     if ( @year % 4 == 0 || @year % 400 == 0 )
-#       return true
-#     elsif ( @year % 100 != 0  )
-#       return false
-#     end
-#   end
-
-#   def zeller(month, day)
-#     months = ["march", "april", "may", "june", "july", "august", "september", "october", "november", "december", "january", "february"]
-#     weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-#     h = day
-#     mo = months.index(month.downcase) + 1 #makes 12 months
-#     yr = (mo <= 10) ? year : year-1
-#     k = yr % 100
-#     x = yr / 100
-#     formula = (h + (((13*mo) - 1) / 5).floor + k + (k/4).floor + (x/4).floor - (2*x)) % 7
-#     weekdays[formula]
-#   end
-# end
-
-  #months code
+  def initialize(m, d, y, month, year)
+    super(m, d, y)
+    @month = month
+    @year = year
+  end
 
   months = ["0", "01","02", "03", "04", "05", "06", "07", "08", "09", "10", "11","12"]
   if ARGV[0] == months[1]
@@ -102,7 +111,7 @@ year = ARGV[1]
   else
     print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29\n"
   end
-
+end
 
 
 
