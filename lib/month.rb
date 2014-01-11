3063d53e4cfb091b8933f5feea22de5ca7739a98
Arielle Adams <arielle919@gmail.com>
2014-01-10 18:50:28 -0600

    month.rb cleaned..reg months print, and leap year months print great

--- lib/month.rb
+++ lib/month.rb
@@ -12,11 +12,106 @@ attr_reader :year, :month
    "June", "July", "August", "September", "October", "November","December"]
 
     if month == months[2] && "#{year}".to_i % 400 != 0 && "#{year}".to_i % 4 != 0
-      print "#{month}".center(20) + "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n"
+      print "#{month} #{year}".center(20)+"\nSu Mo Tu We Th Fr Sa\n"
+        i = 0
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 7
+        end
+        print "\n"
+        i = 7
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 14
+        end
+        print "\n"
+        i = 14
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 21
+        end
+        print "\n"
+        i = 21
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 28
+        end
     elsif month == months[2] && "#{year}".to_i % 4 == 0 && "#{year}".to_i % 400 != 0
-      print "#{month}".center(20) + "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29\n"
+      print "#{month} #{year}".center(20)+"\nSu Mo Tu We Th Fr Sa\n"
+        i = 0
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 7
+        end
+        print "\n"
+        i = 7
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 14
+        end
+        print "\n"
+        i = 14
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 21
+        end
+        print "\n"
+        i = 21
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 28
+        end
+        print "\n"
+        i = 28
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 29
+        end
     elsif month == months[2] && "#{year}".to_i % 400 == 0 && "#{year}".to_i % 4 == 0
-      print "#{month}".center(20) + "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29\n"
+      print "#{month} #{year}".center(20)+"\nSu Mo Tu We Th Fr Sa\n"
+        i = 0
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 7
+        end
+        print "\n"
+        i = 7
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 14
+        end
+        print "\n"
+        i = 14
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 21
+        end
+        print "\n"
+        i = 21
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 28
+        end
+        print "\n"
+        i = 28
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 29
+        end
     else
       print"no month"
     end
@@ -61,105 +156,48 @@ attr_reader :year, :month
           break if i >= 31
         end
       when "April","June","September","November"
-        print "#{month}".center(20) + "\nSu Mo Tu We Th Fr Sa\n 1  2  3  4  5  6  7\n 8  9  10 11 12 13 14\n 15 16 17 18 19 20 21\n 22 23 24 25 26 27 28\n 29 30\n"
+              print "#{month} #{year}".center(20)+"\nSu Mo Tu We Th Fr Sa\n"
+        i = 0
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 7
+        end
+        print "\n"
+        i = 7
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 14
+        end
+        print "\n"
+        i = 14
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 21
+        end
+        print "\n"
+        i = 21
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 28
+        end
+        print "\n"
+        i = 28
+        loop do
+          i += 1
+          print "#{i}".center(3)
+          break if i >= 30
+        end
       else
+      print "this is not a month"
     end
   end
 
 end
 
-# weekdays = ["Sunday", "Monday", "Tuesday",
-#     "Wednesday", "Thursday", "Friday", "Saturday"]
-
-#   months = ["0", "January","February", "March", "April", "May",
-#    "June", "July", "August", "September", "October", "November","December"]
-
-#   if ARGV[0] == months[1]
-#     print "January #{year}".center(20)
-#   end
-#   if ARGV[0] == months[2]
-#     print "February #{year}".center(20)
-#   end
-#   if ARGV[0] == months[3]
-#     print "March #{year}".center(20)
-#   end
-#   if ARGV[0] == months[4]
-#     print "April #{year}".center(20)
-#   end
-#   if ARGV[0] == months[5]
-#     print "May #{year}".center(20)
-#   end
-#   if ARGV[0] == months[6]
-#     print "June #{year}".center(20)
-#   end
-#   if ARGV[0] == months[7]
-#     print "July #{year}".center(20)
-#   end
-#   if ARGV[0] == months[8]
-#     print "August #{year}".center(20)
-#   end
-#   if ARGV[0] == months[9]
-#     print "September #{year}".center(20)
-#   end
-#   if ARGV[0] == months[10]
-#     print "October #{year}".center(20)
-#   end
-#   if ARGV[0] == months[11]
-#     print "November #{year}".center(20)
-#   end
-#   if ARGV[0] == months[12]
-#     print "December #{year}".center(20)
-#   end
-
-#     case ARGV[0]
-#    when "January", "March", "May", "July", "August", "October","December"
-#     print "\nSu Mo Tu We Th Fr Sa\n 1  2  3  4  5  6  7\n 8  9 10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30 31\n"
-#    when "April","June","September","November"
-#     print "\nSu Mo Tu We Th Fr Sa\n 1  2  3  4  5  6  7\n 8  9  10 11 12 13 14\n 15 16 17 18 19 20 21\n 22 23 24 25 26 27 28\n 29 30\n"
-#    else
-#    end
-
-  # if month == months[2] && "#{year}".to_i % 400 != 0 && "#{year}".to_i % 4 != 0
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n"
-  # elsif month == months[2] && "#{year}".to_i % 4 == 0 && "#{year}".to_i % 400 != 0
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29\n"
-  # elsif month == months[2] && "#{year}".to_i % 400 == 0 && "#{year}".to_i % 4 == 0
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29\n"
-  # else
-
-  # end
-
-  # if month == months[1] #January
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30 31\n"
-  # elsif month == months[3]#march
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30 31\n"
-  # elsif month == months[5]#may
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30 31\n"
-  # elsif month == months[7]#july
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30 31\n"
-  # elsif month == months[8]#august
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30 31\n"
-  # elsif month == months[10]#october
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30 31\n"
-  # elsif month == months[12]#december
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30 31\n"
-  # elsif month == months[2] && "#{year}".to_i % 400 != 0 && "#{year}".to_i % 4 != 0
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n"
-  # elsif month == months[2] && "#{year}".to_i % 4 == 0 && "#{year}".to_i % 400 != 0
-  #   print "\nSu Mo Tu We Th Fr Sa\n         1  2  3  4\n5  6  7  8  9  10 11\n12 13 14 15 16 17 18\n19 20 21 22 23 24 25\n26 27 28 29\n"
-  # elsif month == months[2] && "#{year}".to_i % 400 == 0 && "#{year}".to_i % 4 == 0
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29\n"
-  # elsif month == months[4]#April
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30\n"
-  # elsif month == months[6]#June
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30\n"
-  # elsif month == months[9]#September
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30\n"
-  # elsif month == months[11]#November
-  #   print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30\n"
-  # else
-  #   # print "\nSu Mo Tu We Th Fr Sa\n1  2  3  4  5  6  7\n8  9  10 11 12 13 14\n15 16 17 18 19 20 21\n22 23 24 25 26 27 28\n29 30 31\n"
-  # end
 
 
 
